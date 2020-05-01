141ea784fb6a491dcf0abc74f6021f19a0f0bc2b
dawidbaron4 <dawidbaron4@gmail.pl>
2020-05-02 01:34:14 +0200

    adding collapsible sessions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { appRoutes } from './routes';
 import { Error404Component } from './error/404.component';
 import { AuthService } from './user/auth.service';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { CollapsibleWellComponent } from './common/collapsible-well/collapsible-well.component';
 
 @NgModule({
   imports: [
@@ -37,7 +38,8 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
     CreateEventComponent,
     Error404Component,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    CollapsibleWellComponent
   ],
   providers:[
     EventService,
